48f06d44e941ec5c48120ba73fff5ffd03a9cfcf
AJSPARK31 <88500955+AJSPARK31@users.noreply.github.com>
2021-11-21 22:07:10 +0530

    Add files via upload

---
+++ PYTHON FUNCTION TO SUM ALL THE NUMBERS IN A LIST.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[34]:
+
+
+my_list=[8,2,3,0,7]
+def sum(my_list):
+    total=0
+    for i in my_list:
+        total += i
+    return(total)
+print('sum of all element in a list',sum(my_list))
+
+
+# In[33]:
+
+
+
+
+
+# In[27]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
